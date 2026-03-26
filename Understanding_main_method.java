978f9a0d2ab20505b278567eaeefa8e674ff5b52
jayesh-exe <jsyeshsharma@gmail.com>
2026-03-26 19:10:44 +0530

    sixth commit

--- Understanding_main_method.java
+++ Understanding_main_method.java
@@ -1,7 +1,15 @@
 class Test{
-synchronized final public static void main(String[] args)   
+//synchronized final 
+public static void main(String[] args)   
  {
   System.out.println("postmortem of main method in java");
+  System.out.println("main method in java not called by jvm");
   System.out.println(args[0]+args[1]);
+  Test.main(10);
  }
+public static void main(int i)   
+ {
+  System.out.println("main method in java not called by jvm");
+ }
+
 }
