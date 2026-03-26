17bc5e55ef47dbebc5bf8ad247129d6fb36b4bff
jayesh-exe <jsyeshsharma@gmail.com>
2026-03-26 19:03:19 +0530

    fifth commit

---
+++ Understanding_main_method.java
@@ -0,0 +1,7 @@
+class Test{
+synchronized final public static void main(String[] args)   
+ {
+  System.out.println("postmortem of main method in java");
+  System.out.println(args[0]+args[1]);
+ }
+}
