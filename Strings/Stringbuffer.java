d32aa152d41a8173d1663e800a4d517c43106fe6
jayesh-exe <jsyeshsharma@gmail.com>
2026-04-13 19:00:37 +0530

    11th commit

---
+++ Strings/Stringbuffer.java
@@ -0,0 +1,11 @@
+class Testsb
+{
+ public static void main(String[] args){
+ StringBuffer sb1 = new StringBuffer("Cybrom");
+ StringBuffer sb2 = new StringBuffer("Cybrom");
+
+ System.out.println(sb1==sb2);
+ System.out.println(sb1.equals(sb2));
+ }
+
+}
